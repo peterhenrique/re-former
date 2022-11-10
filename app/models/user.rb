188f353b2ab09192e2a7778f9824fa24c3d7700d
peterhenrique <pedrohsdeoliveira27@gmail.com>
2022-11-10 13:33:01 -0300

    edit and update working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: {in: 4..10}
-    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true,  length: {in: 4..10}
+    validates :email, presence: true
     validates :password, presence: true, length: {in: 4..10}
 end
